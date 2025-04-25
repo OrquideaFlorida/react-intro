99a5680f475c097def3d288154ec84a3ae516ae5
OrquideaFlorida <orquideasilva09@yahoo.com.br>
2025-04-25 20:27:41 -0300

    aula de sexta dores de cabeca

--- banco-de-dados/salvandobanco.js
+++ banco-de-dados/salvandobanco.js
@@ -8,14 +8,17 @@ app.use(require("cors")());
 app.use(express.json());
 
 app.get('/', (req, res, next) => {
+    res.json(cadastros);
+})
+
+app.post('/cadastro', (req, res, next) => {
     console.log("Cadastro recebido!");
     //salva no banco de dados
     cadastros.push({
         nome: req.body.txtNome,
-        idade: parseInt(req.body,txtIdade),
+        idade: parseInt(req.body.txtIdade),
         uf: req.body.cmbUF
     });
-    res.json({message: "Tudo ok por aqui!", dados: cadastros});    
-});
-
+    res.json({message: "Tudo ok por aqui!", dados: cadastros});
+})
 app.listen(3001, () => console.log("Servidor escutando na porta 3001..."));
